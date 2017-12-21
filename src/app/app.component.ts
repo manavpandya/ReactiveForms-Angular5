0f6f28c40768f3faf779af6c85e89d3544bc5aab
pandya.manav@gmail.com <pandya.manav@gmail.com>
2017-12-21 10:12:09 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Reactive Form With Angular 5 Demo';
+  rForm: FormGroup;
+  post: any;                     // A property for our submitted form
+  description: string = '';
+  name: string = '';
+  titleAlert:string = 'This field is required';
+  constructor(private fb: FormBuilder) {
+    this.rForm = fb.group({
+      'name': [null, Validators.required],
+      'description': [null, Validators.compose([Validators.required, Validators.minLength(30), Validators.maxLength(500)])],
+      'validate': ''
+    });
+  }
+
+  addPost(post) {
+    this.description = post.description;
+    this.name = post.name;
+  }
+
 }
